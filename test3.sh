f39f27d88408e26fc937b09628c31d65a8a1a3c1
Youngjo Min <tuna10516@kaist.ac.kr>
2022-06-06 20:16:32 +0900

    .

--- test3.sh
+++ test3.sh
@@ -1,8 +1,7 @@
 rm dramsim.log
-nums={98765432198765432 98765432119876543 12345678912345678 98765478945698765}
 
 STARTTIME=$(date +%s)
-../../../pin -t obj-intel64/memorySystem.so -- ./obj-intel64/MatMul.exe  #without RAIDR
+../../../pin -t obj-intel64/memorySystem.so -- ./obj-intel64/fibonacci.exe 98765432198765432  #without RAIDR
 ENDTIME=$(date +%s)
 (( without+=(t2-t1)  ))
 mv dramsim.log dramsim_without_RAIDR.log
@@ -11,12 +10,8 @@ echo "$(($ENDTIME - $STARTTIME)) seconds, test without RAIDR ended"
 
 
 STARTTIME=$(date +%s)
-../../../pin -t obj-intel64/memorySystem_RAIDR.so -- ./obj-intel64/MatMul.exe  #with RAIDR
+../../../pin -t obj-intel64/memorySystem_RAIDR.so -- ./obj-intel64/fibonacci.exe 98765432198765432  #without RAIDR
 ENDTIME=$(date +%s)
 (( without+=(t2-t1)  ))
 mv dramsim.log dramsim_with_RAIDR.log
 echo "$(($ENDTIME - $STARTTIME)) seconds, test with RAIDR ended"
-
-
-../../../pin -t obj-intel64/memorySystem.so -- ./obj-intel64/fibonacci.exe 123456987654 #without RAIDR
-../../../pin -t obj-intel64/memorySystem_RAIDR.so -- ./obj-intel64/fibonacci.exe 123456987654 #with RAIDR
